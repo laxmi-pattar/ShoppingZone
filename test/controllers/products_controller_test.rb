5c0924153a1db7d5e9a72dbc0e73ca2fc2fe207a
Laxmi Pattar <laxmipattar710@gmail.com>
2019-09-25 17:36:19 +0530

    Tested the pages_controllers and product_controllers

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @product = products(:one)
-  end
+ 
 
   test "should get index" do
     get products_url
