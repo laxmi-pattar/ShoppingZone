4733a503062b2a5b3d96ebc285fd6b4e71be4a90
Laxmi Pattar <laxmipattar710@gmail.com>
2019-09-26 15:25:47 +0530

    Implemented system testing for product and address

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,47 +1,23 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  setup do
-    @product = products(:one)
-  end
+
 
   test "visiting the index" do
     visit products_url
-    assert_selector "h1", text: "Products"
+    
   end
 
-  test "creating a Product" do
+  test "should show cart page" do
     visit products_url
-    click_on "New Product"
-
-    fill_in "Description", with: @product.description
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
-    click_on "Create Product"
-
-    assert_text "Product was successfully created"
-    click_on "Back"
+    click_on "Cart"
+    visit pages_cart_url
   end
 
-  test "updating a Product" do
+  test "should show aboutme page" do
     visit products_url
-    click_on "Edit", match: :first
-
-    fill_in "Description", with: @product.description
-    fill_in "Price", with: @product.price
-    fill_in "Title", with: @product.title
-    click_on "Update Product"
-
-    assert_text "Product was successfully updated"
-    click_on "Back"
+    click_on "AboutMe"
+    visit pages_about_url
   end
 
-  test "destroying a Product" do
-    visit products_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Product was successfully destroyed"
-  end
 end
